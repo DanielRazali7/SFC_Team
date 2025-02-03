64ebe3b0574bc1f87b36c9f978f89394b6cd90d8
DanielRazali7 <dnldnn23@gmail.com>
2025-02-04 00:11:47 +0800

    fixing the header and player image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,21 @@ const Header = () => {
   return (
     <div className="header">
       <nav className="menu">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d7/Selangor_FC_logo.svg/800px-Selangor_FC_logo.svg.png"
+          alt="Logo"
+          className="SFClogo"
+        />
         <a href="#">Home</a>
+        <a href="#">News</a>
+        <a href="#">Fixtures</a>
         <a href="#">About</a>
-        <a href="#">Contact</a>
+        <a href="#">Squad</a>
+        <a href="#">Partners</a>
+        <a href="#">Fans</a>
       </nav>
       <div className="subheader-info">
-        <h1>SFC Squad List</h1>
+        <h1>SFC Squad</h1>
         <h2>This is the SFC players for the 2024/25 season</h2>
       </div>
     </div>
